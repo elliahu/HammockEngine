42eac6a7752aaa37ca6e2e1de8f6b109f88fba57
Matej Elias <elias.matysek@gmial.com>
2022-10-22 20:59:22 +0200

    model loading .obj format

--- HammockEngine/HmckModel.cpp
+++ HammockEngine/HmckModel.cpp
@@ -1,5 +1,22 @@
 #include "HmckModel.h"
 
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
+namespace std
+{
+	template <>
+	struct hash<Hmck::HmckModel::Vertex>
+	{
+		size_t operator()(Hmck::HmckModel::Vertex const& vertex) const
+		{
+			size_t seed = 0;
+			Hmck::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+			return seed;
+		}
+	};
+}
+
 Hmck::HmckModel::HmckModel(HmckDevice& device, const HmckModel::Builder& builder) : hmckDevice{ device }
 {
 	createVertexBuffers(builder.vertices);
@@ -16,6 +33,15 @@ Hmck::HmckModel::~HmckModel()
 		vkDestroyBuffer(hmckDevice.device(), indexBuffer, nullptr);
 		vkFreeMemory(hmckDevice.device(), indexBufferMemory, nullptr);
 	}
+}
+
+
+std::unique_ptr<Hmck::HmckModel> Hmck::HmckModel::createModelFromFile(HmckDevice& device, const std::string& filepath)
+{
+	Builder builder{};
+	builder.loadModel(filepath);
+
+	return std::make_unique<HmckModel>(device, builder);
 }
 
 void Hmck::HmckModel::draw(VkCommandBuffer commandBuffer)
@@ -139,4 +165,78 @@ std::vector<VkVertexInputAttributeDescription> Hmck::HmckModel::Vertex::getAttri
 		{0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, position)},
 		{1, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, color)}
 	};
+}
+
+void Hmck::HmckModel::Builder::loadModel(const std::string& filepath)
+{
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string warn, err;
+
+	if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str()))
+	{
+		throw std::runtime_error(warn + err);
+	}
+
+	vertices.clear();
+	indices.clear();
+
+	std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
+	for (const auto &shape : shapes)
+	{
+		for (const auto& index : shape.mesh.indices)
+		{
+			Vertex vertex{};
+
+			if (index.vertex_index >= 0)
+			{
+				vertex.position = {
+					attrib.vertices[3 * index.vertex_index + 0],
+					attrib.vertices[3 * index.vertex_index + 1],
+					attrib.vertices[3 * index.vertex_index + 2]
+				};
+
+				auto colorIndex = 3 * index.vertex_index + 2;
+				if (colorIndex < attrib.colors.size())
+				{
+					vertex.color = {
+						attrib.colors[colorIndex - 0],
+						attrib.colors[colorIndex - 1],
+						attrib.colors[colorIndex - 2]
+					};
+				}
+				else
+				{
+					vertex.color = { 1.f , 1.f, 1.f };
+				}
+			}
+
+			if (index.vertex_index >= 0)
+			{
+				vertex.normal = {
+					attrib.normals[3 * index.normal_index + 0],
+					attrib.normals[3 * index.normal_index + 1],
+					attrib.normals[3 * index.normal_index + 2]
+				};
+			}
+
+			if (index.texcoord_index >= 0) 
+			{
+				vertex.uv = {
+					attrib.texcoords[2 * index.texcoord_index + 0],
+					attrib.texcoords[2 * index.texcoord_index + 1],
+				};
+			}
+
+			if (uniqueVertices.count(vertex) == 0)
+			{
+				uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+				vertices.push_back(vertex);
+			}
+
+			indices.push_back(uniqueVertices[vertex]);
+		}
+	}
 }
--- HammockEngine/HmckModel.h
+++ HammockEngine/HmckModel.h
@@ -1,14 +1,19 @@
 #pragma once
 #include "HmckDevice.h"
+#include "HmckUtils.h"
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
+#define GLM_ENABLE_EXPRIMENTAL
+#include <glm/gtx/hash.hpp>
 
 
 #include <vector>
 #include <cassert>
 #include <cstring>
+#include <memory>
+#include <unordered_map>
 
 namespace Hmck
 {
@@ -20,21 +25,36 @@ namespace Hmck
 		// dont forget to update Vertex::getAttributeDescriptions() to match
 		struct Vertex 
 		{
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position{};
+			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+			bool operator ==(const Vertex& other) const
+			{
+				return 
+					position == other.position &&
+					color == other.color &&
+					normal == other.normal &&
+					uv == other.uv;
+			}
 		};
 
 		struct Builder 
 		{
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+
+			void loadModel(const std::string& filepath);
 		};
 
 		HmckModel(HmckDevice& device, const HmckModel::Builder& builder);
 		~HmckModel();
+
+		static std::unique_ptr<HmckModel> createModelFromFile(HmckDevice& device, const std::string& filepath);
 
 		// delete copy constructor and copy destructor
 		HmckModel(const HmckModel&) = delete;
