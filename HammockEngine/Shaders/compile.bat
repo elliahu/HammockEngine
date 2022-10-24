401b0ff3c1e8c46ca1d00aeaca6d819dde455cdf
Matej Elias <elias.matysek@gmial.com>
2022-10-24 20:15:37 +0200

    shader compiling script updated

--- HammockEngine/Shaders/compile.bat
+++ HammockEngine/Shaders/compile.bat
@@ -1,9 +1,7 @@
+set COMPILER=C:/VulkanSDK/1.3.224.1/Bin/glslc.exe
+set INPUT=.\Raw
+set OUTPUT=.\Compiled
 
-
-C:/VulkanSDK/1.3.224.1/Bin/glslc.exe ./Raw/shader.vert -o ./Compiled/shader.vert.spv
-C:/VulkanSDK/1.3.224.1/Bin/glslc.exe ./Raw/shader.frag -o ./Compiled/shader.frag.spv
-
-C:/VulkanSDK/1.3.224.1/Bin/glslc.exe ./Raw/point_light.vert -o ./Compiled/point_light.vert.spv
-C:/VulkanSDK/1.3.224.1/Bin/glslc.exe ./Raw/point_light.frag -o ./Compiled/point_light.frag.spv
+for /f %%f in ('dir /b .\Raw') do %COMPILER% %INPUT%\%%f -o %OUTPUT%\%%f.spv
 
 pause
