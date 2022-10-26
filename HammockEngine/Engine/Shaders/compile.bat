e8105be1399d8083fa4426ba515fd760f4c07bf6
Matej Elias <elias.matysek@gmial.com>
2022-10-26 20:38:46 +0200

    custom build step

--- HammockEngine/Engine/Shaders/compile.bat
+++ HammockEngine/Engine/Shaders/compile.bat
@@ -1,7 +1,10 @@
-set COMPILER=C:/VulkanSDK/1.3.224.1/Bin/glslc.exe
-set INPUT=.\Raw
-set OUTPUT=.\Compiled
+set COMPILER=C:\VulkanSDK\1.3.224.1\Bin\glslc.exe
+set INPUT=C:\Users\matej\Documents\Projects\Personal\HammockEngine\HammockEngine\Engine\Shaders\Raw
+set OUTPUT=C:\Users\matej\Documents\Projects\Personal\HammockEngine\HammockEngine\Engine\Shaders\Compiled
 
-for /f %%f in ('dir /b .\Raw') do %COMPILER% %INPUT%\%%f -o %OUTPUT%\%%f.spv
+for /f %%f in ('dir /b %INPUT%') do %COMPILER% %INPUT%\%%f -o %OUTPUT%\%%f.spv
 
-pause
+
+
+
+EXIT 0
