d22cd324e0189928d5a4eb17e548c25767730338
Matěj Eliáš <matej.elias@outlook.cz>
2022-10-31 21:17:03 +0100

    changed my mind

---
+++ HammockEngine/Engine/Utils/HmckUtils.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+#include <functional>
+
+namespace Hmck
+{
+	// dark magic from: https://stackoverflow.com/a/57595105
+	template <typename T, typename... Rest>
+	void hashCombine(std::size_t& seed, const T& v, const Rest&... rest) {
+		seed ^= std::hash<T>{}(v)+0x9e3779b9 + (seed << 6) + (seed >> 2);
+		(hashCombine(seed, rest), ...);
+	};
+
+} // namespace Hmck
