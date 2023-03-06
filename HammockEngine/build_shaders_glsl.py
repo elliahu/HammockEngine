e9097ee00555a58e632e93c98369f2c8df5310b3
elda <elias.matysek@gmail.com>
2023-03-06 01:46:45 +0100

    build shaders scripts added

---
+++ HammockEngine/build_shaders_glsl.py
@@ -0,0 +1,18 @@
+# Build glsl shaders to SPIR-V format
+
+import os
+import subprocess
+
+compiler = os.environ['VULKAN_SDK'] + '\Bin\glslc.exe'
+input = 'Engine\Shaders\Raw\glsl'
+output = 'Engine\Shaders\Compiled'
+
+# Iterate shaders
+for path in os.listdir(input):
+    shader_in = os.path.abspath(os.path.join(input, path))
+    shader_out = os.path.abspath(os.path.join(output, path)) + '.spv'
+    # Check if current path is a file
+    if os.path.isfile(os.path.join(input, path)):
+        #compile
+        subprocess.check_call([compiler, shader_in, '-o', shader_out])
+        print(path + ' compiled successfully')
--- HammockEngine/compile_shaders.bat
+++
@@ -1,7 +0,0 @@
-set COMPILER=%VULKAN_SDK%\Bin\glslc.exe
-set INPUT=.\Engine\Shaders\Raw\glsl
-set OUTPUT=.\Engine\Shaders\Compiled
-
-for /f %%f in ('dir /b %INPUT%') do %COMPILER% %INPUT%\%%f -o %OUTPUT%\%%f.spv
-
-pause
