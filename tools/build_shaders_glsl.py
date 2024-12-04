d2507c80f7ff75427834d21a240b1a0a8ad6ee3e
Matej Elias <matej.elias@vsb.cz>
2024-12-04 20:08:51 +0100

    updated shader build script

--- tools/build_shaders_glsl.py
+++ tools/build_shaders_glsl.py
@@ -1,18 +1,28 @@
-# Build glsl shaders to SPIR-V format
-
 import os
 import subprocess
+import platform
 
-compiler = os.environ['VULKAN_SDK'] + '/Bin/glslc.exe'
+# Determine the operating system
+if platform.system() == 'Windows':
+    compiler = os.environ['VULKAN_SDK'] + '\\Bin\\glslc.exe'
+elif platform.system() == 'Linux':
+    compiler = 'glslc'
+else:
+    raise EnvironmentError("Unsupported OS")
+
 raw_shaders = r'../src/engine/shaders/raw/glsl'
 compiled_shaders = r'../src/engine/shaders/compiled'
+
+# Ensure the compiled shaders directory exists
+os.makedirs(compiled_shaders, exist_ok=True)
 
 # Iterate shaders
 for path in os.listdir(raw_shaders):
     shader_in = os.path.abspath(os.path.join(raw_shaders, path))
     shader_out = os.path.abspath(os.path.join(compiled_shaders, path)) + '.spv'
+
     # Check if current path is a file
-    if os.path.isfile(os.path.join(raw_shaders, path)):
-        #compile
-        subprocess.check_call([compiler, shader_in, '-o', shader_out, '--target-env=vulkan1.2', '--target-spv=spv1.4']) 
+    if os.path.isfile(shader_in):
+        # Compile
+        subprocess.check_call([compiler, shader_in, '-o', shader_out, '--target-env=vulkan1.2', '--target-spv=spv1.4'])
         print(path + ' compiled successfully')
