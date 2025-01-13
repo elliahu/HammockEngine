868b100bcf9144bf0cc69f0921ac9887a888d6cd
Matěj Eliáš <matej.elias@vsb.cz>
2025-01-13 21:36:33 +0100

    barrier class for transitions

--- include/hammock/core/RenderGraph.h
+++ include/hammock/core/RenderGraph.h
@@ -10,7 +10,6 @@
 
 namespace hammock {
     namespace rendergraph {
-
         /**
         * Describes general buffer
         */
@@ -81,7 +80,8 @@ namespace hammock {
             enum class Type {
                 Buffer,
                 Image,
-                SwapChain // Special type of image. If resource is SwapChain, it is the final output resource that gets presented.
+                SwapChain
+                // Special type of image. If resource is SwapChain, it is the final output resource that gets presented.
             } type;
 
             // Name is used for lookup
@@ -116,7 +116,8 @@ namespace hammock {
             enum class Type {
                 Graphics, // Represents a pass that draws into some image
                 Compute, // Represents a compute pass
-                Transfer // Represents a transfer pass - data is moved from one location to another (eg. host -> device or device -> host)
+                Transfer
+                // Represents a transfer pass - data is moved from one location to another (eg. host -> device or device -> host)
             } type; // Type of the pass
 
             std::string name; // Name of the pass for debug purposes and lookup
@@ -129,23 +130,134 @@ namespace hammock {
 
 
         /**
-         * Describes image barrier that will transform an image from one layout to another
+         * Wraps necessary operation needed for pipeline barrier transitions for a single resource
          */
-        struct ImageBarrier {
-            std::string resourceName;
-            VkImageLayout oldLayout;
-            VkImageLayout newLayout;
-            VkAccessFlags srcAccessFlags;
-            VkAccessFlags dstAccessFlags;
-            VkPipelineStageFlags srcStageFlags;
-            VkPipelineStageFlags dstStageFlags;
+        class Barrier {
+            ResourceNode &node;
+            ResourceAccess &access;
+            VkCommandBuffer commandBuffer;
+            uint32_t frameIndex = 0;
+
+        public:
+            explicit Barrier(ResourceNode &node, ResourceAccess &access, VkCommandBuffer commandBuffer,
+                             const uint32_t frameIndex) : node(node),
+                                                          access(access), commandBuffer(commandBuffer),
+                                                          frameIndex(frameIndex) {
+            }
+
+            /**
+             *  Checks if resource needs barrier transition give its access
+             * @return true if resource needs barrier transition, else false
+             */
+            bool isNeeded() const {
+                if (node.type == ResourceNode::Type::Image) {
+                    ASSERT(std::holds_alternative<ImageResourceRef>(node.refs[frameIndex].resource),
+                           "BarrierManager::needsBarrier sanity check: Node is image node yet does not hold image ref.")
+                    ;
+                    ImageResourceRef ref = std::get<ImageResourceRef>(node.refs[frameIndex].resource);
+                    return ref.currentLayout != access.requiredLayout;
+                }
+                // TODO support for buffer transition
+                return false;
+            }
+
+            /**
+             * Applies pipeline barrier transition
+             */
+            void apply() {
+                if (node.type == ResourceNode::Type::Image) {
+                    // Verify that we're dealing with an image resource
+                    ASSERT(std::holds_alternative<ImageResourceRef>(node.refs[frameIndex].resource),
+                           "Barrier::apply sanity check: Node is image node yet does not hold image ref.");
+
+                    // Get the image reference for the current frame
+                    ImageResourceRef &ref = std::get<ImageResourceRef>(node.refs[frameIndex].resource);
+
+                    // Skip if no transition is needed, this should already be checked but anyway...
+                    if (ref.currentLayout == access.requiredLayout) {
+                        return;
+                    }
+
+                    // Create image memory barrier
+                    VkImageMemoryBarrier imageBarrier{};
+                    imageBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+                    imageBarrier.oldLayout = ref.currentLayout;
+                    imageBarrier.newLayout = access.requiredLayout;
+                    imageBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+                    imageBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+                    imageBarrier.image = ref.image;
+
+                    // Define image subresource range
+                    imageBarrier.subresourceRange.aspectMask =
+                    isDepthStencil(ref.desc.format)
+                        ? VK_IMAGE_ASPECT_DEPTH_BIT
+                        : VK_IMAGE_ASPECT_COLOR_BIT;
+                    imageBarrier.subresourceRange.baseMipLevel = 0;
+                    imageBarrier.subresourceRange.levelCount = ref.desc.mips;
+                    imageBarrier.subresourceRange.baseArrayLayer = 0;
+                    imageBarrier.subresourceRange.layerCount = ref.desc.layers;
+
+                    // Set access masks based on layout transitions
+                    switch (ref.currentLayout) {
+                        case VK_IMAGE_LAYOUT_UNDEFINED:
+                            imageBarrier.srcAccessMask = 0;
+                            break;
+                        case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
+                            imageBarrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+                            break;
+                        case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
+                            imageBarrier.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+                            break;
+                        case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
+                            imageBarrier.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+                            break;
+                        case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
+                            imageBarrier.srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+                            break;
+                        default:
+                            imageBarrier.srcAccessMask = 0;
+                    }
+
+                    switch (access.requiredLayout) {
+                        case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
+                            imageBarrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+                            break;
+                        case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
+                            imageBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+                            break;
+                        case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
+                            imageBarrier.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+                            break;
+                        case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
+                            imageBarrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+                            break;
+                        default:
+                            imageBarrier.dstAccessMask = 0;
+                    }
+
+                    // Issue the pipeline barrier command
+                    vkCmdPipelineBarrier(
+                        commandBuffer,  // commandBuffer
+                        access.stageFlags, // srcStageMask
+                        access.stageFlags, // dstStageMask
+                        0, // dependencyFlags
+                        0, nullptr, // Memory barriers
+                        0, nullptr, // Buffer memory barriers
+                        1, &imageBarrier // Image memory barriers
+                    );
+
+                    // Update the current layout
+                    ref.currentLayout = access.requiredLayout;
+                }
+                // TODO support for buffer transition
+            }
         };
 
         /**
          * RenderGraph is a class representing a directed acyclic graph (DAG) that describes the process of creation of a frame.
          * It consists of resource nodes and render pass nodes. Render pass node can be dependent on some resources and
          * can itself create resources that other passes may depend on. RenderGraph analyzes these dependencies, makes adjustments when possible,
-         * makes necessary transitions between resource states.
+         * makes necessary transitions between resource states. These transitions are done just in time as well as resource allocation.
          *  TODO support for Read Modify Write - render pass writes and reads from the same resource
          */
         class RenderGraph {
@@ -153,6 +265,8 @@ namespace hammock {
             uint32_t maxFramesInFlight; // Maximal numer of frames that GPU works on concurrently
             std::unordered_map<std::string, ResourceNode> resources; // Holds all the resources
             std::vector<RenderPassNode> passes; // Holds all the render passes
+            std::vector<uint32_t> optimizedOrderOfPasses;
+            // Array of indexes into list of passes. Order is optimized by the optimizer.
             uint32_t rootPass = 0; // Pass from which the rendering starts (top of the graph)
 
             /**
@@ -223,6 +337,11 @@ namespace hammock {
              */
             void compile() {
                 // TODO we are now making no optimizations and assume that rendering goes in order of render pass submission
+
+                // To be replaced when above task is implemented
+                for (int i = 0; i < passes.size(); i++) {
+                    optimizedOrderOfPasses.push_back(i);
+                }
             }
 
 
@@ -232,7 +351,33 @@ namespace hammock {
              * @param frameIndex Index of the current frame.This is used to identify which resource ref should be used of the resource node is buffered.
              */
             void execute(VkCommandBuffer cmd, uint32_t frameIndex) {
+                ASSERT(optimizedOrderOfPasses.size() == passes.size(),
+                       "RenderGraph::execute sanity check: RenderPass queue size don't match. This should not happen.");
 
+                for (const auto &passIndex: optimizedOrderOfPasses) {
+                    RenderPassNode &pass = passes[passIndex];
+
+                    // Check for necessary transitions of input resources
+                    for (auto &access: pass.inputs) {
+                        ResourceNode &resourceNode = resources[access.resourceName];
+
+                        // Apply barrier if it is needed
+                        if (Barrier barrier(resourceNode, access, cmd, frameIndex); barrier.isNeeded()) {
+                            barrier.apply();
+                        }
+                    }
+
+                    // Check for necessary transitions of output resources
+                    // TODO this might need some adjustments
+                    for (auto &access: pass.outputs) {
+                        ResourceNode &resourceNode = resources[access.resourceName];
+
+                        // Apply barrier if it is needed
+                        if (Barrier barrier(resourceNode, access, cmd, frameIndex); barrier.isNeeded()) {
+                            barrier.apply();
+                        }
+                    }
+                }
             }
         };
     }
